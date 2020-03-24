c8e7e9e182076c9f51015d7a43c6421e7077bfaa
Fenris <anatolijv236@gmail.com>
2020-03-24 22:43:43 +0300

    add hover, tab and start slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
 $(document).ready(() => {
+    /**
+     * TABS
+     */
+    let item = $('.tab');
+    let itemContent = $('.tab__content');
 
+
+
+    item.on('click', function () {
+        console.log(123);
+        if (!$(this).hasClass('tab_active')) {
+            console.log(456);
+            item.removeClass("tab_active");
+            itemContent.fadeOut(500);
+            itemContent.delay(500).eq($(this).index()).fadeIn(500);
+            $(this).addClass("tab_active");
+        }
+    });
+
+    /**
+     * SLICK SLIDER
+     */
+    let slider = $('.firstSlider');
+
+    if(slider.length > 0) {
+
+        slider.slick({
+            responsive: [{
+                breakpoint: 480,
+                settings: {
+                    arrows: false
+                }
+            },
+            ]
+        });
+    }
 });
