166199aabb7acda92629b44d48a8a40eae311e3d
Fenris <anatolijv236@gmail.com>
2020-03-28 20:38:12 +0300

    add scripts for select, slider, date. fix adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,12 @@ $(document).ready(() => {
     let itemContent = $('.tab__content');
 
 
-
-    item.on('click', function () {
-        console.log(123);
-        if (!$(this).hasClass('tab_active')) {
-            console.log(456);
+    item.on('click', (e) => {
+        if (!$(e.target).hasClass('tab_active')) {
             item.removeClass("tab_active");
             itemContent.fadeOut(500);
             itemContent.delay(500).eq($(this).index()).fadeIn(500);
-            $(this).addClass("tab_active");
+            $(e.target).addClass("tab_active");
         }
     });
 
@@ -23,7 +20,7 @@ $(document).ready(() => {
      */
     let slider = $('.firstSlider');
 
-    if(slider.length > 0) {
+    if (slider.length > 0) {
 
         slider.slick({
             arrows: false,
@@ -37,4 +34,45 @@ $(document).ready(() => {
             ]
         });
     }
+
+    let itemsSlider = $('.tab__content__wrapper');
+
+    if (itemsSlider.length > 0) {
+
+        itemsSlider.slick({
+            infinite: true,
+            arrows: true,
+            dots: false,
+            slidesToShow: 5,
+            autoplay: false,
+            responsive: [
+                {
+                    breakpoint: 1100,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                }
+            ]
+        });
+    }
+
+    $('#city').select2();
+    $('#store').select2();
+
+    // $.datepicker.setDefaults( $.datepicker.regional[ "" ] );
+    $("#date").datepicker({
+        dateFormat: "dd-mm-yy",
+        minDate: new Date($('#hiddendelivdate').val()),
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        dayNamesMin: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    });
+    $("#anim").on("change", function () {
+        $("#datepicker").datepicker("option", "showAnim", 'fadeIn');
+    });
 });
