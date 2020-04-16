e9cad6f9fa9d024caa150d63db230cdf587bf99a
Fenris <anatolijv236@gmail.com>
2020-04-16 21:48:49 +0300

    fixes

--- js/scripts.js
+++ js/scripts.js
@@ -62,6 +62,14 @@ $(document).ready(() => {
             });
         }
 
+        /**
+         * HAMBURGER
+         */
+        $('.menu-toggle-inner').on('click', function (e) {
+            $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+            $('.headerNav_adaptive').toggleClass('header_opened');
+        });
+
         $('#city').select2();
         $('#store').select2();
 
