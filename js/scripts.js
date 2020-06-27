8a0ca4ea3c93da35bc8be8f2b9ed17ed8e107108
Fenris <anatolijv236@gmail.com>
2020-06-27 16:29:05 +0300

    adaptive for about page

--- js/scripts.js
+++ js/scripts.js
@@ -144,15 +144,21 @@ $(document).ready(() => {
         autoplay: false,
         responsive: [
             {
-                breakpoint: 1100,
+                breakpoint: 1000,
                 settings: {
-                    slidesToShow: 4,
+                    slidesToShow: 3,
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
                 }
             }
         ]
